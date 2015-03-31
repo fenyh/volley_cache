a468d7190c106f1234de9c9cb243fc5eb162d22b
fenyh <qizu@sohu.com>
2015-03-31 14:16:05 +0800

    ---------------初始化导入----

---
+++ src/com/storm/ui/MainActivity.java
@@ -0,0 +1,76 @@
+package com.storm.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.fengyh.volley.cache.main.RequestContent;
+import com.fengyh.volley.cache.util.Configs;
+import com.fengyh.volley.cache.util.DebugUtil;
+import com.fengyh.volley.cache.util.JsonUtil;
+import com.fengyh.volley.cache.util.NetWorkHandler;
+import com.storm.customloading.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity{
+
+	private Button but;
+	
+	public static enum CacheTime{
+		Time(Configs.TIME_HOUR),Dat(Configs.TIME_DAY);
+		private int time;
+		private CacheTime(int time){
+			this.time = time;
+		}
+	}
+	
+	private NetWorkHandler handler = new NetWorkHandler(Looper.myLooper()) {
+		@Override
+		protected void onNetRequestSuccess(Message msg) {
+			DebugUtil.d("---onNetRequestSuccess---", msg.obj+"");
+		}
+		
+		protected void onCacheRequestSuccess(Message msg) {
+			DebugUtil.d("---onCacheRequestSuccess---", msg.obj+"");
+		};
+		
+		@Override
+		protected void onNetRequestFailure(Message msg) {
+			
+		}
+	};
+	/**
+	 * 生产地址
+	 */
+	public static final String BASE_TEST_POST_URL = "https://mgateway.cmbchinaucs.com/Interface.aspx";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		super.setContentView(R.layout.activity_main);
+		Button but = (Button) findViewById(R.id.but);
+		but.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				/*Map<String, String> params = new HashMap<String, String>();
+				params.put("FtCode", "1");
+				RequestContent content = new RequestContent(MainActivity.this);
+				//content.getRequestContent(BASE_TEST_POST_URL, 80000, params, handler, new NetcontentHandler());
+				//content.getRequestContent(BASE_TEST_POST_URL, 80000, params, "123456", handler, new NetcontentHandler());
+				
+				content.getRequestContentMemoryIntel(BASE_TEST_POST_URL, 80000, params, "123456", handler, new NetcontentHandler());
+			*/
+			
+			}
+		});
+	}
+}
---
+++ src/com/storm/ui/NetcontentHandler.java
@@ -0,0 +1,23 @@
+package com.storm.ui;
+
+import android.os.Message;
+
+import com.fengyh.volley.cache.main.HttpStringContentHandler;
+import com.fengyh.volley.cache.util.DebugUtil;
+import com.fengyh.volley.cache.util.NetWorkHandler;
+
+public class NetcontentHandler extends HttpStringContentHandler{
+
+	@Override
+	public void onPraseJson(String jsonStr, boolean isCache,
+			NetWorkHandler handler) {
+		DebugUtil.d("--onPraseJson----", jsonStr);
+		Message msg = handler.obtainMessage();
+		msg.obj = jsonStr;
+		if(!isCache){
+			handler.sendSuccessCacheMesage(msg);
+		}else{
+			handler.sendSuccessMesage(msg);
+		}
+	}
+}
